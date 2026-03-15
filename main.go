066b731d342ef1d50be71abfc5b70f4cd2410cda
yuri-potatoq <yuri.ylr@outlook.com>
2026-03-15 14:58:56 -0300

    code quality refactor

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
@@ -10,43 +14,108 @@ import (
 	"github.com/gen2brain/beeep"
 )
 
+var (
+	targetDir string
+	debug     bool
+
+	defaultDir = filepath.Join(os.Getenv("HOME"), "Downloads")
+
+	defaultImagesDir = "Images"
+	defaultVideoDir  = "Vídeos"
+	defaultAudioDir  = "Músicas"
+
+	suffixDirMapping = map[string]string{
+		// For images
+		"jpeg": defaultImagesDir, "jpg": defaultImagesDir, "png": defaultImagesDir,
+		// For audios
+		"mp4": defaultVideoDir, "mkv": defaultVideoDir, "mov": defaultVideoDir,
+		// For videos
+		"mp3": defaultAudioDir, "wav": defaultAudioDir, "aac": defaultAudioDir,
+	}
+)
+
 func main() {
-	watcher, _ := fsnotify.NewWatcher()
+	flag.StringVar(&targetDir, "target-dir", defaultDir, `Directory from which the app should watch events.`)
+	flag.StringVar(&defaultImagesDir, "images-dir", defaultImagesDir, `Directory where we move image files.`)
+	flag.StringVar(&defaultAudioDir, "audios-dir", defaultAudioDir, `Directory where we move audio files.`)
+	flag.StringVar(&defaultVideoDir, "videos-dir", defaultVideoDir, `Directory where we move video files.`)
+	flag.BoolVar(&debug, "debug", false, `Enable debuging logging. By default is disabled`)
+	flag.Parse()
+
+	handlerOpts := &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+	if debug {
+		handlerOpts.Level = slog.LevelDebug
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, handlerOpts))
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error("Aconteceu algum error irrecuperável", "error", err)
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		logger.ErrorContext(ctx, "não foi possível criar um 'watcher' para o path", "target-dir", targetDir)
+	}
 	defer watcher.Close()
+	
+	err = watcher.Add(targetDir)
+	if err != nil {
+		logger.ErrorContext(ctx, "Não foi possível escutar os eventos da pasta", "target-dir", targetDir)
+		return
+	}
 
-	path := filepath.Join(os.Getenv("HOME"), "Downloads")
-	watcher.Add(path)
+	logger.InfoContext(ctx, "Iniciando rotina de verificação: ",
+		"target-dir", targetDir,
+		"images-dir", defaultImagesDir,
+		"audios-dir", defaultAudioDir,
+		"videos-dir", defaultVideoDir,
+		"debug", debug)
+	run_watch(ctx, logger, watcher)
+}
 
+func run_watch(ctx context.Context, logger *slog.Logger, watcher *fsnotify.Watcher) {
 	for {
 		select {
 		case event := <-watcher.Events:
 			if event.Op&fsnotify.Create == fsnotify.Create {
-				nomeArquivo := event.Name
-				home, _ := os.UserHomeDir()
-				// ESSE É PARA AS IMAGENS
-				if strings.HasSuffix(nomeArquivo, ".jpeg") || strings.HasSuffix(nomeArquivo, ".jpg") || strings.HasSuffix(nomeArquivo, ".png") {
-					// MUDE O "IMAGENS" PARA O NOME DA PASTA DESEJADA QUE FIQUE AS IMAGENS, UM EXEMPLO: PICTURES
-					destino := filepath.Join(home, "Imagens", filepath.Base(nomeArquivo))
-					time.Sleep(2 * time.Second)
-					os.Rename(nomeArquivo, destino)
-					beeep.Notify("Organizador Go", "Arquivos organizado! 📸", "")
-					// ESSE É PARA OS VIDEOS
-				} else if strings.HasSuffix(nomeArquivo, ".mp4") || strings.HasSuffix(nomeArquivo, ".mkv") || strings.HasSuffix(nomeArquivo, ".mov") {
-					// Troque "Vídeos" pelo nome da sua pasta de videos
-					destino := filepath.Join(home, "Vídeos", filepath.Base(nomeArquivo))
-					time.Sleep(2 * time.Second)
-					os.Rename(nomeArquivo, destino)
-					beeep.Notify("Organizador Go", "Arquivos organizado! 📸", "")
-					// Esse é para musicas/audios
-				} else if strings.HasSuffix(nomeArquivo, ".mp3") || strings.HasSuffix(nomeArquivo, ".wav") || strings.HasSuffix(nomeArquivo, ".aac") {
-					// troque "músicas" pelo nome da sua pasta de musicas/audios
-					destino := filepath.Join(home, "Músicas", filepath.Base(nomeArquivo))
-					time.Sleep(2 * time.Second)
-					os.Rename(nomeArquivo, destino)
-					beeep.Notify("Organizador Go", "Arquivos organizado! 📸", "")
+				fileName := event.Name
+				slog.Debug("Novo arquivo criado em", "file-name", fileName)
+
+				// we do that in case of there is any another "." on the file path like:
+				// "/path/to/video.file.map"
+				lastSuffixIdx := strings.LastIndex(fileName, ".")
+
+				// no prefix with dot found
+				if lastSuffixIdx == -1 {
+					// TODO: use linux utility to find the type of the file
+					// `> file <file_name>`
+					logger.InfoContext(ctx, "Sufixo do tipo de arquivo não reconhecido", "file-name", fileName)
+					continue
 				}
+
+				suffix := fileName[lastSuffixIdx+1:]
+				folderTargetName, ok := suffixDirMapping[suffix]
+				if !ok {
+					logger.InfoContext(ctx, "Sufixo do tipo de arquivo não reconhecido", "file-name", fileName)
+					continue
+				}
+
+				destPath := filepath.Join(targetDir, folderTargetName, filepath.Base(fileName))
+				time.Sleep(2 * time.Second)
+				os.Rename(fileName, destPath)
+				beeep.Notify("Organizador Go", fmt.Sprintf("Arquivos na pasta [%s => %s] organizados! 📸", fileName, destPath), "")
 			}
-		case <-watcher.Errors:
+		case evt := <-watcher.Errors:
+			logger.ErrorContext(ctx, "Evento de cancelamento recebido", "error", evt.Error())
+			return
+		case <-ctx.Done():
+			logger.InfoContext(ctx, "Evento de cancelamento do context")
 			return
 		}
 	}
